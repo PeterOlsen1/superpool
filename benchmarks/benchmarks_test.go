04fb4afe1e1db58ba45b960618c28069ea82e0d1
PeterOlsen1 <peterolsen455@gmail.com>
2025-12-22 15:28:39 -0600

    test: update benchmark tests, dealing with deadlock

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -10,41 +10,43 @@ import (
 var taskCount int = 10000
 
 func dummyTask(i int) error {
-	// Simulate work (e.g., 1ms sleep)
 	time.Sleep(1 * time.Millisecond)
 	return nil
 }
 
-func BenchmarkSuperpool(b *testing.B) {
+// BenchmarkPool-16    	      10	 111145265 ns/op	   93129 B/op	     544 allocs/op
+func BenchmarkPool(b *testing.B) {
 	poolSize := uint16(50)
 
-	for n := 0; n < b.N; n++ {
+	for b.Loop() {
 		pool, _ := sp.NewPool(1000, poolSize, dummyTask)
-		for i := 0; i < taskCount; i++ {
+		for range taskCount {
 			pool.Add(1)
 		}
-		pool.Wait()
+		pool.Shutdown()
 	}
 }
 
+// BenchmarkGoroutines-16    	     196	   6082681 ns/op	 1143147 B/op	   20061 allocs/op
 func BenchmarkGoroutines(b *testing.B) {
-	for n := 0; n < b.N; n++ {
+	for b.Loop() {
 		done := make(chan struct{}, taskCount)
-		for i := 0; i < taskCount; i++ {
+		for range taskCount {
 			go func() {
 				dummyTask(1)
 				done <- struct{}{}
 			}()
 		}
-		for i := 0; i < taskCount; i++ {
+		for range taskCount {
 			<-done
 		}
 	}
 }
 
+// BenchmarkSequential-16    	       1	10705755996 ns/op	     176 B/op	       5 allocs/op
 func BenchmarkSequential(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for i := 0; i < taskCount; i++ {
+	for b.Loop() {
+		for range taskCount {
 			dummyTask(1)
 		}
 	}
