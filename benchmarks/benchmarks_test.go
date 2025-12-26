9f16b18dff930f73aacf228679945985a7580288
Peter Olsen <olse0321@umn.edu>
2025-12-26 16:51:59 -0600

    test: update benchmark tests

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -1,34 +1,56 @@
 package benchmarks
 
 import (
+	"net/http"
 	"testing"
-	"time"
 
 	sp "github.com/PeterOlsen1/superpool"
 )
 
-var taskCount int = 10000
+var taskCount int = 100
+var server *http.Server
+
+func startLocal() {
+	server = &http.Server{Addr: "127.0.0.1:8000"}
+	go func() {
+		server.ListenAndServe()
+	}()
+}
+
+func stopLocal() {
+	if server != nil {
+		server.Shutdown(nil)
+	}
+}
 
 func dummyTask(i int) error {
-	time.Sleep(1 * time.Millisecond)
+	http.Get("127.0.0.1:8000")
 	return nil
 }
 
 // BenchmarkPool-16    	      10	 111145265 ns/op	   93129 B/op	     544 allocs/op
 func BenchmarkPool(b *testing.B) {
-	poolSize := uint16(50)
+	startLocal()
+	defer stopLocal()
+
+	numWorkers := uint16(50)
+	poolSize := uint32(1000)
+	pool, _ := sp.NewPool(poolSize, numWorkers, dummyTask)
+	defer pool.Shutdown()
 
 	for b.Loop() {
-		pool, _ := sp.NewPool(1000, poolSize, dummyTask)
 		for range taskCount {
 			pool.Add(1)
 		}
-		pool.Shutdown()
+		pool.Wait()
 	}
 }
 
 // BenchmarkGoroutines-16    	     196	   6082681 ns/op	 1143147 B/op	   20061 allocs/op
 func BenchmarkGoroutines(b *testing.B) {
+	startLocal()
+	defer stopLocal()
+
 	for b.Loop() {
 		done := make(chan struct{}, taskCount)
 		for range taskCount {
@@ -45,6 +67,9 @@ func BenchmarkGoroutines(b *testing.B) {
 
 // BenchmarkSequential-16    	       1	10705755996 ns/op	     176 B/op	       5 allocs/op
 func BenchmarkSequential(b *testing.B) {
+	startLocal()
+	defer stopLocal()
+
 	for b.Loop() {
 		for range taskCount {
 			dummyTask(1)
